099726bbc2c5ae39f6946eec6ece85c249231008
parkdecl <parkdecl+kr.github@gmail.com>
2018-02-18 16:36:26 +0900

    run.js 결과를 comma 구분된 문자열로 출력

--- run.js
+++ run.js
@@ -6,11 +6,10 @@ Line2Chain.config('sample');
 
 Line2Chain.getChain('weekday')
   .then(chain => {
-    console.log(chain);
-    // console.log('train_num,from,dest,prev_train,next_train');
-    // for(const c of chain) {
-    //   console.log(`${c.train_num},${c.from},${c.dest},${c.prev_train},${c.next_train}`);
-    // }
+    console.log('train_num,from,dest,prev_train,next_train');
+    for(const c of chain) {
+      console.log(`${c.train_num},${c.from},${c.dest},${c.prev_train},${c.next_train}`);
+    }
   })
   .catch(e => {
     console.log(e);
